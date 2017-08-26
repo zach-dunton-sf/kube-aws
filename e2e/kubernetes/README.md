34df35c6742c1fb3579a196d4e276b156d361141
Maxim Ivanov <ivanov.maxim@gmail.com>
2017-08-26 08:43:22 +0100

    fresh start

--- e2e/kubernetes/README.md
+++
@@ -1,1 +0,0 @@
-`DOCKER_REPO=... make build publish` to build and then push the docker image with the latest, pre-built Kubernetes E2E test suite.
--- model/ec2_instance.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type EC2Instance struct {
-	Count         int    `yaml:"count,omitempty"`
-	CreateTimeout string `yaml:"createTimeout,omitempty"`
-	InstanceType  string `yaml:"instanceType,omitempty"`
-	RootVolume    `yaml:"rootVolume,omitempty"`
-	Tenancy       string `yaml:"tenancy,omitempty"`
-}
